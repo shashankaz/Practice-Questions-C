d2b9fce8e565a2a5855b3379e81cd1d912325f73
Shashank <113260615+shashankaz@users.noreply.github.com>
2022-12-20 01:02:53 +0530

    Add files via upload

---
+++ Pattern Printing 3.c
@@ -0,0 +1,20 @@
+//Pattern printing
+
+#include <stdio.h>
+int main() 
+{
+   int i, j, rows;
+   
+   printf("Enter the Number of Rows : ");
+   scanf("%d", &rows);
+   
+   for (i = 1; i <= rows; i++) 
+   {
+      for (j = 1; j <= i; j++) 
+      {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
