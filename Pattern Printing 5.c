93038880aa8ff35e713594c82d01ad92aa15947a
Shashank <113260615+shashankaz@users.noreply.github.com>
2022-12-23 20:43:15 +0530

    Add files via upload

---
+++ Pattern Printing 5.c
@@ -0,0 +1,32 @@
+//Pattern printing
+
+#include <stdio.h>
+//Pattern printing
+
+int main() 
+{
+    int x,y,n,k;
+    
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    
+    for(x=1;x<=n;x++)
+    {
+        for(y=1;y<=x;y++)
+        {
+            printf("%d",y);
+        }
+        printf("\n");
+    }
+    
+    for(k=n;k>=1;k--)
+    {
+        for(y=1;y<k;y++)
+        {
+            printf("%d",y);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
